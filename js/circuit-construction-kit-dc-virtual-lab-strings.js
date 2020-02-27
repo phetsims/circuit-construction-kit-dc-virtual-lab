218b88151b1fdb684aea9a49c7d9e8fae2c08a63
samreid <reids@colorado.edu>
2020-02-27 11:39:58 -0700

    ES6 module migration, see https://github.com/phetsims/chipper/issues/875

---
+++ js/circuit-construction-kit-dc-virtual-lab-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import circuitConstructionKitDcVirtualLab from './circuitConstructionKitDcVirtualLab.js';
+
+const circuitConstructionKitDcVirtualLabStrings = getStringModule( 'CIRCUIT_CONSTRUCTION_KIT_DC_VIRTUAL_LAB' );
+
+circuitConstructionKitDcVirtualLab.register( 'circuitConstructionKitDcVirtualLabStrings', circuitConstructionKitDcVirtualLabStrings );
+
+export default circuitConstructionKitDcVirtualLabStrings;
