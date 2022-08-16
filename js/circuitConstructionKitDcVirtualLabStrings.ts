273a57b08975522935a35be6d668583944596b84
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:32 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/circuitConstructionKitDcVirtualLabStrings.ts
+++ js/circuitConstructionKitDcVirtualLabStrings.ts
@@ -5,11 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import circuitConstructionKitDcVirtualLab from './circuitConstructionKitDcVirtualLab.js';
 
 type StringsType = {
   'circuit-construction-kit-dc-virtual-lab': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   }
 };
 
