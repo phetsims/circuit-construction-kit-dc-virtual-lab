cbc75a9648961075cdf58ee45e87f9531b3d08ee
Jesse <jesse.greenberg@colorado.edu>
2022-08-24 12:58:04 -0400

    Rename CCKCOptionsDialogContent.ts -> CCKCGeneralPreferencesContentNode.ts, see https://github.com/phetsims/circuit-construction-kit-common/issues/890

--- js/circuit-construction-kit-dc-virtual-lab-main.js
+++ js/circuit-construction-kit-dc-virtual-lab-main.js
@@ -7,7 +7,7 @@
  */
 
 import CCKCConstants from '../../circuit-construction-kit-common/js/CCKCConstants.js';
-import CCKCOptionsDialogContent from '../../circuit-construction-kit-common/js/view/CCKCOptionsDialogContent.js';
+import CCKCGeneralPreferencesContentNode from '../../circuit-construction-kit-common/js/view/CCKCGeneralPreferencesContentNode.js';
 import LabScreen from '../../circuit-construction-kit-dc/js/lab/LabScreen.js';
 import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
 import Sim from '../../joist/js/Sim.js';
@@ -27,7 +27,7 @@ const simOptions = {
   preferencesModel: new PreferencesModel( {
     generalOptions: {
       customPreferences: [ {
-        createContent: tandem => new CCKCOptionsDialogContent( tandem.createTandem( 'cckPreferencesContent' ) )
+        createContent: tandem => new CCKCGeneralPreferencesContentNode( tandem.createTandem( 'cckPreferencesContent' ) )
       } ]
     }
   } ),
