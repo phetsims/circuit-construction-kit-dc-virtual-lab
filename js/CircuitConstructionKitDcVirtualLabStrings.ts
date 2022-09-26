ca71df2c8dc439f0e03841ee6ba10a3e1cc8dca0
Sam Reid <reids@colorado.edu>
2022-09-26 12:11:17 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/CircuitConstructionKitDcVirtualLabStrings.ts
+++ js/CircuitConstructionKitDcVirtualLabStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import circuitConstructionKitDcVirtualLab from './circuitConstructionKitDcVirtualLab.js';
 
 type StringsType = {
   'circuit-construction-kit-dc-virtual-lab': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 
