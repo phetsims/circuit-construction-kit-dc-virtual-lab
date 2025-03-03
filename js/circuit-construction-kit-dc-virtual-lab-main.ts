8ec5688a8b2818617d642a8ecf8fd86be83fc0a3
matthew-blackman <mabl4992@colorado.edu>
2025-03-03 13:11:19 -0500

    Update LTA model to incorporate noise, upate resistanceWithNoiseProperty, remove stale options - see https://github.com/phetsims/circuit-construction-kit-common/issues/1002

--- js/circuit-construction-kit-dc-virtual-lab-main.ts
+++ js/circuit-construction-kit-dc-virtual-lab-main.ts
@@ -25,11 +25,7 @@ simLauncher.launch( () => {
   // Launch the simulation once everything is ready
   const sim = new Sim( circuitConstructionKitDcVirtualLabTitleStringProperty, [
     new LabScreen( tandem.createTandem( 'labScreen' ), {
-      showNoncontactAmmeters: false,
-      showAdvancedControls: false,
-
-      // Uncomment to show charts on DC Virtual Lab
-      showCharts: true
+      showNoncontactAmmeters: false
     } )
   ], {
     preferencesModel: new PreferencesModel( {
