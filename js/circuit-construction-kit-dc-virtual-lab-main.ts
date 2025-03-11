a0c7dab0f3d011c1aabb70cf15d9b9591ed4f091
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-11 16:57:50 -0600

    add CCKCSim and fix iframe keyboard bug in all CCK sims, https://github.com/phetsims/circuit-construction-kit-common/issues/1027

--- js/circuit-construction-kit-dc-virtual-lab-main.ts
+++ js/circuit-construction-kit-dc-virtual-lab-main.ts
@@ -6,10 +6,10 @@
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import CCKCSim from '../../circuit-construction-kit-common/js/view/CCKCSim.js';
 import CCKCSimulationPreferencesContentNode from '../../circuit-construction-kit-common/js/view/CCKCSimulationPreferencesContentNode.js';
 import LabScreen from '../../circuit-construction-kit-dc/js/lab/LabScreen.js';
 import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
-import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import soundManager from '../../tambo/js/soundManager.js';
 import Tandem from '../../tandem/js/Tandem.js';
@@ -23,7 +23,7 @@ const circuitConstructionKitDcVirtualLabTitleStringProperty = CircuitConstructio
 simLauncher.launch( () => {
 
   // Launch the simulation once everything is ready
-  const sim = new Sim( circuitConstructionKitDcVirtualLabTitleStringProperty, [
+  const sim = new CCKCSim( circuitConstructionKitDcVirtualLabTitleStringProperty, [
     new LabScreen( tandem.createTandem( 'labScreen' ), {
       showNoncontactAmmeters: false
     } )
