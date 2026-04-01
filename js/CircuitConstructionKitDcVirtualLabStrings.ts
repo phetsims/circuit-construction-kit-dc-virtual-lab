d42e5d9e5b42bfa3a353b4c13508972df1dc10c5
phet-dev <phetadmin@gmail.com>
2026-04-01 16:23:58 -0600

    grunt update from daily grunt work

--- js/CircuitConstructionKitDcVirtualLabStrings.ts
+++ js/CircuitConstructionKitDcVirtualLabStrings.ts
@@ -19,4 +19,6 @@ type StringsType = {
 
 const CircuitConstructionKitDcVirtualLabStrings = getStringModule( 'CIRCUIT_CONSTRUCTION_KIT_DC_VIRTUAL_LAB' ) as StringsType;
 
+circuitConstructionKitDcVirtualLab.register( 'CircuitConstructionKitDcVirtualLabStrings', CircuitConstructionKitDcVirtualLabStrings );
+
 export default CircuitConstructionKitDcVirtualLabStrings;
